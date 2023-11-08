a23bfb660d672abeb3b81c977660bc664da47682
gusanitor8 <gusanitor8@gmail.com>
2023-11-08 12:25:56 -0600

    functional version

---
+++ db.pl
@@ -0,0 +1,23 @@
+% Definición del grafo dirigido
+grafo([a, b, c, d], [(a, b), (a, c), (a, d), (c, d)]).
+
+% Predicado para buscar un camino en el grafo
+camino(X, Y, Camino) :-
+    grafo(_, Arcos),          % Obtenemos los arcos del grafo
+    camino(X, Y, Arcos, [], Camino).
+
+% Caso base: encontramos un camino
+camino(X, X, _, Camino, [X|Camino]).
+
+% Caso recursivo: buscamos un camino
+camino(X, Y, Arcos, Visitados, Camino) :-
+    member((X, Z), Arcos),          % Elegimos un arco (X, Z)
+    write('X: '), write(X), nl,
+    write('Z: '), write(Z), nl,
+    \+ member(Z, Visitados),      % Z no debe estar en los visitados
+    write('ACCEPTED'),nl,
+    camino(Z, Y, Arcos, [X|Visitados], Camino).
+
+
+
+
